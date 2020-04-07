3faeac9bc4b89d2785c41407f128d63b18acc424
salesforcesan <salesforce.san@gmail.com>
2020-04-07 12:20:34 -0500

    bash

--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -6,8 +6,9 @@ if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -d $DURATION
-sfdx force:source:push
-sfdx force:user:permset:assign -n dreamhouse
+sfdx force:org:create --setdefaultusername -a santechorg -s -f config/project-scratch-def.json -d $DURATION
+sfdx force:source:push -u dev-scratch-org
+sfdx force:user:permset:assign -n santechorg
 sfdx force:org:open -p /lightning/page/home
 echo "Org is set up"
+sfdx force:data:tree:import --plan data/export-demo-Broker__c-plan.json -u dev-scratch-org
